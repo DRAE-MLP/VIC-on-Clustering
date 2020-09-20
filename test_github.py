9d6b6d4ee7fc1ffe8f81632e7332752e8294382f
Ramon <62185126+Ramon-Diaz@users.noreply.github.com>
2020-09-20 13:20:45 -0500

    test deleted

--- test_github.py
+++
@@ -1,7 +0,0 @@
-print('Hello world')
-print('Your code is all right')
-
-for element in range(2):
-    print('THis is the element: '+str(element))
-
-print('This is another example of the version 2.0')
